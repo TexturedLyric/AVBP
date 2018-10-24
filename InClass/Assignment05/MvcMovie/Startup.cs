75c336dbaec8e13cf7c19522f443dcea3a1a85ef
Michael Matthews <mamatthews1@buffs.wtamu.edu>
2018-10-24 18:27:46 -0500

    update on assignment 05 in inclass

--- InClass/Assignment05/MvcMovie/Startup.cs
+++ InClass/Assignment05/MvcMovie/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -24,6 +26,10 @@ namespace MvcMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddDbContext<MvcMovieContext>(options =>
+                  options.UseSqlite("Data Source=MvcMovie.db"));
+                  
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
