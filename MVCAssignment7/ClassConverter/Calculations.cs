a8ec9bf47acbb288414cc0ea40c91c29071d2416
Michael Matthews <mamatthews1@buffs.wtamu.edu>
2018-11-14 20:08:48 -0600

    assignment7 update

---
+++ MVCAssignment7/ClassConverter/Calculations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ClassConverter
+{
+    public class Class1
+    {
+        public decimal CelToFar(decimal value)
+        {
+            return (9m/5m)*(value)+32;
+        }
+        public decimal FarToCel(decimal value)
+        {
+            return (5m/9m)*(value-32);
+        }
+        public decimal FeetToYard(decimal value)
+        {
+            return value/3;
+        }
+        public decimal YardToFeet(decimal value)
+        {
+            return value*3;
+        }
+        public decimal FeetToMeter(decimal value)
+        {
+            return value/(1250/381);
+        }
+        public decimal MeterToFeet(decimal value)
+        {
+            return value*(1250/381);
+        }
+        public decimal OunceToPound(decimal value)
+        {
+            return value/16;
+        }
+        public decimal PoundToOunce(decimal value)
+        {
+            return value*16;
+        }
+        public decimal OunceToGram(decimal value)
+        {
+            return value*28;
+        }
+        public decimal GramToOunce(decimal value)
+        {
+            return value/28;
+        }
+        public decimal GramToKilo(decimal value)
+        {
+            return value/1000;
+        }
+        public decimal KiloToGram(decimal value)
+        {
+            return value*1000;
+        }
+    }
+}
---
+++ MVCAssignment7/Models/ConverterViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCAssignment7.Models
+{
+    public class ConverterViewModel
+    {
+        public decimal ValueToCon {get; set;}
+        public string ConType {get; set;}
+        public decimal ConValue {get; set;}
+    }
+}
